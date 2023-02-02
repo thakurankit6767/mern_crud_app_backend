81f40aeedf1ef6f7bc0a41e50e99d105577d1e84
thakurankit6767 <thakurankt6767@gmail.com>
2023-02-02 15:46:44 +0530

    add files

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import Routes from './server/route.js';
+import Connection from './database/db.js';
+
+const app = express();
+
+
+dotenv.config();
+
+// To handle HTTP POST requests in Express.js version 4 and above, 
+// you need to install the middleware module called body-parser.
+// body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
+app.use(bodyParser.json({extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/', Routes);
+
+const USERNAME = process.env.DB_CONNECT;
+
+const PORT = process.env.PORT || 8000;
+Connection(USERNAME);
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
